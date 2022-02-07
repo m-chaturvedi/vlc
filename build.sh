bb5bc84dd65ee812b392a8d66f9a352f9c469926
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2022-02-07 17:39:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 
 sudo apt-get install libxcb-shm0-dev libxcb-xv0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-xkb-dev
 echo "deb http://www.deb-multimedia.org bullseye main" >> /etc/apt/sources.list
-sudo apt-get install deb-multimedia-keyring
+sudo apt-get install deb-multimedia-keyring qtdeclarative5-dev
 sudo apt-get install liblivemedia-dev libqt5quickwidgets5 libqt5quickcontrols2-5
 
-./configure --prefix=$PWD/install --disable-a52 --enable-live555 --disable-qt --disable-lua
+./configure --prefix=$PWD/install --disable-a52 --enable-live555 --disable-lua
 
